5376412fef6df7ee305f7efbc1c6f710aa17b8b6
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-02-09 18:11:26 +0300

    Remove copyright notices and more unused code.

--- src/video/nacl/SDL_naclvideo.h
+++ src/video/nacl/SDL_naclvideo.h
@@ -1,24 +1,3 @@
-/*
-    SDL - Simple DirectMedia Layer
-    Copyright (C) 1997-2009 Sam Lantinga
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-    Sam Lantinga
-    slouken@libsdl.org
-*/
 #include "SDL_config.h"
 
 #ifndef _SDL_naclvideo_h
@@ -31,10 +10,6 @@ extern "C" {
 
 #include <ppapi/cpp/instance.h>
 #include <ppapi/cpp/graphics_2d.h>
-// #include <nacl/nacl_imc.h>
-// #include <nacl/nacl_npapi.h>
-// #include <nacl/npapi_extensions.h>
-// #include <nacl/npruntime.h>
 
 
 /* Hidden "this" pointer for the video functions */
